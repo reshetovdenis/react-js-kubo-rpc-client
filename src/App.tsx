38a2c73a3d02d20fe8f3b78722275a771f5fbcca
Reshetov Denis <reshetovdenis@gmail.com>
2023-11-05 19:42:39 +0900

    Working usage of IPFS with local ips node

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { create } from 'kubo-rpc-client'
+
+const client = create({ url: "http://127.0.0.1:5001/api/v0" });
 
 function App() {
+  const [cid, setCid] = useState<string | null>(null);
+
+  const handleButtonClick = async () => {
+    try {
+      const response = await client.add('Hello world!');
+      setCid(response.cid.toString());  // Convert to string here
+      console.log(response.cid);
+    } catch (error) {
+      console.error("Failed to add to client:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <button onClick={handleButtonClick}>Add to Client</button>
+      {cid && <p>CID: {cid}</p>}
     </div>
   );
 }
